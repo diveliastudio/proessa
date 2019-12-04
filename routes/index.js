339996ad36d06dbeddf670a6f5ca17e4f517cb82
Fred Paucar <fred.paucarespinoza@gmail.com>
2019-12-04 17:05:53 -0500

    turning html to ejs

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ const express = require('express'),
   router = express.Router();
 
 router.get('/', function (req, res) {
-  res.render('index', {})
+  res.render('index')
+});
+
+router.get('/services', function (req, res) {
+  res.render('services')
 });
 
 module.exports = router;
