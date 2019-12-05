2c2db2e1f93b51a2d921286e354922d82e649310
Fred Paucar <fred.paucarespinoza@gmail.com>
2019-12-04 23:59:37 -0500

    enabling views

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,18 @@ router.get('/', function (req, res) {
   res.render('index')
 });
 
+router.get('/nosotros', function (req, res) {
+  res.render('about')
+});
+
+router.get('/contactanos', function (req, res) {
+  res.render('contact')
+});
+
+router.get('/consultoria', function (req, res) {
+  res.render('pricing')
+});
+
 router.get('/services', function (req, res) {
   res.render('services')
 });
